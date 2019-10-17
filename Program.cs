a63dcb72c4dac2fd01978079d6c941861117ebce
M-Orlinski <56115992+M-Orlinski@users.noreply.github.com>
2019-10-17 13:59:37 +0200

    Tabliczka mnożenia

--- Program.cs
+++ Program.cs
@@ -1,47 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace choinka_10._10
+namespace Tabliczka_mnożenia
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int n = 10;
+            int row = 1;
+            int col;
 
-            Console.WriteLine("Podaj wysokość choinki: ");
-            string ile = Console.ReadLine();
-            int ile1;
-            Console.WriteLine("Z jakich znaków ma być choinka: ");
-            string znak = Console.ReadLine();
-            char znak1;
-
-
-            if (char.TryParse(znak, out znak1))
+            
+            do
             {
-                if (int.TryParse(ile, out ile1))
+                col = 1;
+                do
                 {
-                    for (int i = 1; i <= ile1; i++)
-                    {
-                        for (int j = 1; j <= i + ile1; j++)
-                        {
-                            if (j > ile1 - i)
-
-                                Console.Write(znak1);
-                            else
-                                Console.Write(" ");
-                        }
-                        Console.Write("\n");
-                    }
+                    Console.Write(row * col + "\t");
+                    col++;
                 }
-                else
-                {
-                    Console.WriteLine("ERROR");
-                }
+                while (col <= n);
+                row++;
+                Console.WriteLine( );
+            }
+            while (row <= 10);
+            
+            Console.ReadKey();
+           
 
-            }
-            else
-            {
-                Console.WriteLine("ERROR");
-            }
+           
         }
     }
 }
