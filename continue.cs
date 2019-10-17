6988f47c541ced91607fb8234c7f2fe74ed9b0b5
M-Orlinski <56115992+M-Orlinski@users.noreply.github.com>
2019-10-17 14:24:45 +0200

    Add files via upload

---
+++ continue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int j = 1;
+            for(; j <=10; j++)
+            {
+                if (j == 2 || j == 5 || j == 7)
+                    continue;
+                else
+                    Console.WriteLine("{0} ", j);
+            }
+            Console.WriteLine();
+            */
+
+            int j = 0;
+            for (; j <= 10; j++)
+            {
+                if (j == 2 || j == 5 || j == 7)
+                    break;
+                else
+                    Console.WriteLine("{0} ", j);
+            }
+            Console.WriteLine();
+        }
+    }
+}
