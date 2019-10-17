c4f098884a5758f29014173ec65d9d388e5e666e
M-Orlinski <56115992+M-Orlinski@users.noreply.github.com>
2019-10-17 14:25:31 +0200

    Add files via upload

---
+++ Alfabet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alfabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char x = 'A';
+            for(; x <= 'Z'; x++)
+            {
+                
+                if(x != 'Z')
+                    Console.Write("{0}, ", x);
+                else
+                    Console.Write("{0}. ", x);
+
+            }
+            Console.Write();
+            x--;
+
+            for(; x >= 'A'; x--)
+            {
+                if(x!='A')
+                        Console.Write("{0}, ", x);
+                    else
+                        Console.Write("{0}. ", x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
